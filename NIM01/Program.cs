c12beabe64c82d540c441b32459b69f759c046f1
Jeff Borg <jeff.borg@apollovideo.com>
2016-01-23 15:52:38 -0800

    Yes/no user input working.

--- NIM01/Program.cs
+++ NIM01/Program.cs
@@ -10,13 +10,50 @@ namespace NIM01
     {
         static void Main(string[] args)
         {
+            //Welcom message
             Console.Clear();
             Console.WriteLine("Hey, it's time to play NIM!");
 
+            //Intantiate the game
             NIM_Game theGame = new NIM_Game();
             
+            //Tell user the number of stones in the pile
             Console.WriteLine("Stones in pile:{0}", theGame.gameStart() );
-            
+
+            //Who goes first
+            Console.Write("Do you want to go first?");
+            char userInput;
+            bool isUserFirst = false;
+            bool isInputValid = false;
+            do
+            {
+                userInput = Console.ReadKey().KeyChar;
+                if ((userInput == 'Y') || (userInput == 'y'))
+                {
+                    isUserFirst = true;
+                    isInputValid = true;
+                }
+                else if ((userInput == 'N') || (userInput == 'n'))
+                {
+                    isUserFirst = false;
+                    isInputValid = true;
+                }
+                else
+                {
+                    isInputValid = false;
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid Input");
+                }
+                    
+            } while (!isInputValid);
+
+            Console.WriteLine();
+
+            if (isUserFirst)
+                Console.WriteLine("User goes first.");
+            else
+                Console.WriteLine("Computer goes first.");
+
         }
     }
 }
